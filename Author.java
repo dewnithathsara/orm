031b9ec9c8902b1a6accc5f9eb1b25ea882c63da
dewnithathsara <139765543+dewnithathsara@users.noreply.github.com>
2024-02-15 08:11:01 +0530

    Update Author.java

--- Author.java
+++ Author.java
@@ -14,17 +14,25 @@ public class Author {
     @OneToMany(mappedBy = "author",cascade = CascadeType.ALL)
 
     private List<Book> book;
-
+    private String country;
     public Author(){
 
     }
 
-    public Author(Long id, String name, List<Book> book) {
+    public Author(Long id, String name, List<Book> book,String country) {
         this.id = id;
         this.name = name;
         this.book = book;
+        this.country=country;
+    }
+    public String getCountry() {
+        return country;
     }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
     public Long getId() {
         return id;
     }
